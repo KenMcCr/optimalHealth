cc65983ae05fbfd4c6ee9ea42633745c1125fc91
Marvin Thapa Renteral <mtrenteral@gmail.com>
2020-12-21 16:50:08 -0500

    fixed external API

--- Finished/client/app.js
+++ Finished/client/app.js
@@ -5,9 +5,8 @@ const x_button = document.querySelector(".x-button");
 const elated = document.querySelector(".elated")
 const tranquil = document.querySelector(".tranquil")
 const gloomy = document.querySelector(".gloomy")
-const elated_songs = document.querySelector("#elated")
-const tranquil_songs = document.querySelector("#tranquil")
-const gloomy_songs = document.querySelector("#gloomy")
+const html_songs = document.querySelector('#playlist_songs');
+const spotBttn = document.querySelectorAll('#spotifyBttn');
 
 
 suggest_button.addEventListener('click', (event) => {
@@ -24,20 +23,79 @@ x_button.addEventListener('click', (event)=>{
     suggest_window.classList.replace("recommendations", "recommendations_hidden")
 })
 
-elated.addEventListener('click', (event) => {
-    elated_songs.classList.toggle('elated_songs_show')
-    elated_songs.classList.toggle('elated_songs')
-})
 
-tranquil.addEventListener('click', (event) => {
-    tranquil_songs.classList.toggle('tranquil_songs_show')
-    tranquil_songs.classList.toggle('tranquil_songs')
-})
 
-gloomy.addEventListener('click', (event) => {
-    gloomy_songs.classList.toggle('gloomy_songs_show')
-    gloomy_songs.classList.toggle('gloomy_songs')
-})
+const playlist_generator = async (event) => {
+
+    var genre = event.target.dataset.genre;
+    var playlist_ID;
+
+    switch (genre){
+        case "tranquil":
+            playlist_ID = "6wObnEPQ63a4kei1sEcMdH"
+            break
+        case "elated":
+            playlist_ID = "37i9dQZF1DWW5pQp5JqjY4"
+            break
+        case "gloomy":
+            playlist_ID = "4C5u13rggsEdRN7An3A6uT"
+            break
+        default:
+            return console.log("something wrong!")
+    }
+
+    console.log(genre)
+
+    const clientId = "9603daa58de14596a5a42fdbd2280c54";
+    const clientSecret = "0f92c144864746bcb889785f4fe1f0fa";
+
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded', 
+            'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+        },
+        body: 'grant_type=client_credentials'
+    });
+  
+    const data = await result.json();
+    const token = data.access_token;
+
+    const playlist_result = await fetch(`https://api.spotify.com/v1/playlists/${playlist_ID}/tracks`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+    const playlist_data = await playlist_result.json();
+    console.log(playlist_data);
+    const playlist_songs = playlist_data.items;
+    const playlist_ul = document.createElement('ul');
+    playlist_songs.forEach(song => {
+        const song_name = song.track.name;
+        const song_li = document.createElement('li')
+        song_li.innerText = song_name;
+        song_li.classList.add('songs')
+        playlist_ul.appendChild(song_li);
+        
+    });
+    html_songs.appendChild(playlist_ul);
+    
+}
+
+spotBttn.forEach((button) => {
+    button.addEventListener('click', playlist_generator);
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const ApCont = (function(){
@@ -65,9 +123,9 @@ gloomy.addEventListener('click', (event) => {
     //         headers: { 'Authorization' : 'Bearer ' + token}
     //     });
 
-//         const data = await result.json();
-//         return data.name;
-//     }
+    //     const data = await result.json();
+    //     return data.name;
+    // }
 
 //     const _getPlaylist = async (token) => {
 //         const limit = 10;
--- Finished/index.js
+++ Finished/index.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const fs = require("fs");
 const SpotifyWebApi = require('spotify-web-api-node');
 const path = require('path')
+const request = require('request')
 
 app.use(express.static(path.join(__dirname, 'client')));
 
@@ -14,50 +15,50 @@ app.use(express.json());
 
 
 
-
-var scopes = ['user-read-private', 'user-read-email'],
-  redirectUri = 'http://localhost:3000',
-  clientId = '9603daa58de14596a5a42fdbd2280c54',
-  state = 'Virginia',
-  showDialog = true,
-  responseType = 'token';
+// var scopes = ['user-read-private', 'user-read-email'],
+//   redirectUri = 'http://localhost:3000',
+//   clientId = '9603daa58de14596a5a42fdbd2280c54',
+//   state = 'Virginia',
+//   showDialog = true,
+//   responseType = 'token';
  
-// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
-  redirectUri: redirectUri,
-  clientId: clientId
-});
+// // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+// var spotifyApi = new SpotifyWebApi({
+//   redirectUri: redirectUri,
+//   clientId: clientId
+// });
  
-// Create the authorization URL
-var authorizeURL = spotifyApi.createAuthorizeURL(
-  scopes,
-  state,
-  showDialog,
-  responseType
-);
- 
-// https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=token&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice&show_dialog=true
-console.log(authorizeURL);
-var spotifyApi = new SpotifyWebApi({
-    // If code does not work, got to http://localhost:3000/login after starting node server, and also copy the link inside the terminal and put in in browser to log in.
-    accessToken: 'BQBKAOGOP2aUZiEca75oTYs_kYrbzdyU3bWVoUukkLmZ8_1PML980UHhPR2HVCao_AvVBtKlpWSUL6R64sZS-K7reDSsM_-OgANlhN48BpB4uaGyXL0odfQPTiscKnA-bhXbH2sz87b7KkOvSJKdrqDE3K8n-qNCGFg3sq2pfRfPhbUQhYk'
-});
+// // Create the authorization URL
+// var authorizeURL = spotifyApi.createAuthorizeURL(
+//   scopes,
+//   state,
+//   showDialog,
+//   responseType
+// );
 
-spotifyApi.getPlaylist('6wObnEPQ63a4kei1sEcMdH')
-  .then(function(data) {
-    console.log('Some information about this playlist', data.body.tracks.items);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-});
+// // https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=token&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice&show_dialog=true
+// console.log(authorizeURL);
+// var spotifyApi = new SpotifyWebApi({
+//     // If code does not work, got to http://localhost:3000/login after starting node server, and also copy the link inside the terminal and put in in browser to log in.
+//     accessToken: 'BQCq6PlR78My0_JFAkDJ0ylsGEsXlvjfXQheXhsAYD6Zm_zNRoEr_p9goh9CIgofinJp5jwmMk3p2ByYt_zXqDKp18EgCbTqWL3YnwVTUYrkUNfnvvgXtvpCjp_7IR9YlSMLvP8wDAjHXelqoJjL5zuga_MWBP8Bt_nreaNimZ0VhxjYmTU'
+// });
 
-// app.get('/login', function(req, res) {
-//     var scopes = 'user-read-private user-read-email';
-//     res.redirect('https://accounts.spotify.com/authorize' +
-//       '?response_type=code' +
-//       '&client_id=' + "9603daa58de14596a5a42fdbd2280c54" +
-//       (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-//       '&redirect_uri=' + encodeURIComponent('http://localhost:3000'));
-//     });
+// spotifyApi.getPlaylist('6wObnEPQ63a4kei1sEcMdH')
+//   .then(function(data) {
+//     console.log('Some information about this playlist', data.body.tracks.items);
+//   }, function(err) {
+//     console.log('Something went wrong!', err);
+// });
+
+// // app.get('/login', function(req, res) {
+// //     var scopes = 'user-read-private user-read-email';
+// //     res.redirect('https://accounts.spotify.com/authorize' +
+// //       '?response_type=code' +
+// //       '&client_id=' + "9603daa58de14596a5a42fdbd2280c54" +
+// //       (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+// //       '&redirect_uri=' + encodeURIComponent('http://localhost:3000'));
+// //     });
+
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/index.html"));
